01df2fc30e22c436a05eba024c63889c186465e7
Sigit Wahyudi <44634025+sgitwhyd@users.noreply.github.com>
2025-05-02 00:46:06 +0700

    feat deploy workflow (#3)
    * feat: ci testing
    
    * refactor(CI): remove test stage
    
    * fix(CI): startup failure
    
    * feat(CI): create deploy pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li>
+            this update from github runners and git actions
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
